08e6e399992f29c4647d0ea3e097694d791d7f8a
stevy.sung <stevy.sung@kakaocorp.com>
2022-05-30 22:36:20 +0900

    김영한 스프링 입문 - AOP가 필요한 상황

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -16,9 +16,17 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
     public Long join(Member member){
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = "+ timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -29,7 +37,14 @@ public class MemberService {
     }
 
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = "+ timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
