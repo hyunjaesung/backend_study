ac7d4289dc2f0dac9152d0753f4419a1303053a5
stevy.sung <stevy.sung@kakaocorp.com>
2022-05-31 07:52:59 +0900

    김영한 스프링 입문 - 적용

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -15,36 +15,21 @@ public class MemberService {
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
-    public Long join(Member member){
 
-        long start = System.currentTimeMillis();
-        try {
-            validateDuplicateMember(member);
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = "+ timeMs + "ms");
-        }
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getName())
-                .ifPresent(m -> { // 옵셔널로 리턴이되서 뭔가 할 수 있다
-                    throw new IllegalStateException("이미 존재하는 회원입니다.");
-                });
+        memberRepository.findByName(member.getName()).ifPresent(m -> { // 옵셔널로 리턴이되서 뭔가 할 수 있다
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
     }
 
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = "+ timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
