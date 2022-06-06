d479c1cd0fbb9b3430e62a276425382e261ac06d
stevy.sung <stevy.sung@kakaocorp.com>
2022-06-06 13:40:42 +0900

    자바의 정석 - 11장 연습문제

--- java-practice/collection/src/Ex11_18.java
+++ java-practice/collection/src/Ex11_18.java
@@ -1,5 +1,36 @@
-public class Ex11_18 {
-    String[] data = {"A", "B", "C", "A", "A", "B", "C", "C"};
-    
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 
+class Student implements Comparable {
+    String name;
+
+    Student(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "이름: " + name;
+    }
+
+    public int compareTo(Object o) {
+        return name.compareTo(((Student) o).name);
+    }
+}
+
+public class Ex11_18 {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(new Student("라구마"));
+        list.add(new Student("노구마"));
+        list.add(new Student("고구마"));
+        list.add(new Student("다구마"));
+
+        Collections.sort(list);
+        Iterator it = list.iterator();
+
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
 }
