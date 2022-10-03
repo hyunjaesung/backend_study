c5a3882de4b17cad9b5bd0c68f0e68dd026039d9
stevy.sung <stevy.sung@kakaocorp.com>
2022-10-03 12:14:54 +0900

    예제 3 예제 4

--- kotlin-start/src/main/kotlin/main.kt
+++ kotlin-start/src/main/kotlin/main.kt
@@ -1,5 +1,3 @@
-import java.util.StringJoiner
-
 fun getPassOrFail(score: Int): String {
     return if (score >= 50) {
         "P"
@@ -40,7 +38,31 @@ fun startsWithA(obj: Any): Boolean {
     }
 }
 
+fun numIter(){
+    val numbers = listOf<Long>(1L, 2L, 3L)
+    for(number in numbers){
+        println(number)
+    }
+}
+
+fun manyForLoop(){
+    for(i in 1..3){
+        println(i)
+    }
+
+    for(i in 1..5 step 2){
+        println(i)
+    }
+
+    for(i in 3 downTo 1){
+        println(i)
+    }
+
+    for(i in 3 downTo 1 step 2){
+        println(i)
+    }
+}
+
 fun main() {
-    println(getGradeWithSwitch(88))
-//    println(startsWithA(12))
+    manyForLoop();
 }
