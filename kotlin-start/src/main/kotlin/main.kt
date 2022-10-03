1d6e6666e9a7e02bf0c62cf26ce618243eaf47fb
stevy.sung <stevy.sung@kakaocorp.com>
2022-10-03 11:41:29 +0900

    예제2

--- kotlin-start/src/main/kotlin/main.kt
+++ kotlin-start/src/main/kotlin/main.kt
@@ -1,13 +1,15 @@
-fun getPassOrFail(score:Int): String{
-    return if(score >= 50){
+import java.util.StringJoiner
+
+fun getPassOrFail(score: Int): String {
+    return if (score >= 50) {
         "P"
-    }else{
+    } else {
         "F"
     }
 }
 
 fun getGrade(score: Int): String {
-    return when{
+    return when {
         score >= 90 -> "A"
         score >= 80 -> "B"
         score >= 70 -> "C"
@@ -15,12 +17,30 @@ fun getGrade(score: Int): String {
     }
 }
 
-fun validate(score: Int){
-   if(score in 0..100){
-       println("")
-   }
+fun validate(score: Int) {
+    if (score in 0..100) {
+        println("")
+    }
+}
+
+fun getGradeWithSwitch(score: Int): String {
+    return when (score / 10) {
+        9 -> "A"
+        8 -> "B"
+        7 -> "C"
+        else -> "D"
+    }
+}
+
+fun startsWithA(obj: Any): Boolean {
+    return if (obj is String) {
+        obj.startsWith("A")
+    } else {
+        return false
+    }
 }
 
 fun main() {
-    println(validate(80))
+    println(getGradeWithSwitch(88))
+//    println(startsWithA(12))
 }
